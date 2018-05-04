6d675c89c20354496e379c8b91597f6615a48f91
seunghyuk <sunghyuk9@naver.com>
2018-05-04 15:29:49 +0900

    QuoteNode & ListNode

---
+++ week06/parser/ast/IntNode.java
@@ -0,0 +1,14 @@
+package parser.ast;
+//201302422 ½Å½ÂÇõ
+public class IntNode implements ValueNode { // »õ·Î ¼öÁ¤µÈ IntNode
+	private Integer value;
+
+	@Override
+	public String toString() {
+		return "INT: " + value;
+	}
+
+	public IntNode(String text) {
+		this.value = new Integer(text);
+	}
+}
---
+++ week06/parser/ast/Node.java
@@ -0,0 +1,5 @@
+package parser.ast;
+//201302422 ½Å½ÂÇõ
+public interface Node {
+	
+}
